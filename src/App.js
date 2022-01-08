f94c4b43d47e13e11666448cd859c5609fac9c15
ghidyon <gideonakunana@gmail.com>
2022-01-08 23:53:28 +0100

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-// import './App.css';
 
 function App() {
 
   return (
-    <div className="App"></div>
+    <div >
+      <h1 className="text-center">Give me a hug</h1>
+    </div>
   );
 }
 
